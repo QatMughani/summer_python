f863c95f74030f2b5874276d1526ac9c4fa9619d
QatMughani <qatmughani@gmail.com>
2023-05-08 04:17:25 -0400

    This is week 1 first commit

---
+++ w1/as_calculator.py
@@ -0,0 +1,19 @@
+# multiplication *
+# syntax error: if something is missing or meaning is incomplete i.e., missing end bracket
+# semantic error: meaning is defined but  answer is invalid, i.e., 4/0
+
+
+
+if __name__ == "__main__":
+    print(2 + 3)  # addition +
+    print(2 - 3)  # negative -
+    print(2 / 3)  # division /
+    print(2 // 3)  # integer division //
+    print(2 ** 3)  # exponential **
+    print(4 % 2)  # modulo or remainder %
+    print(4 - 3 * 5)   # preference of
+    # solving algebraic expression: bracket exponential product...
+    print(-4 - 2 ** 3)  # preference
+    # of solving algebraic expression: bracket exponential product...
+    print((3 + 4) * 3)  # preference
+    # of solving algebraic expression: bracket exponential product...
